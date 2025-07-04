f065a74366493e4f706cf882ae137214234ca13e
Jonathan0823 <eguinjonathan08@gmail.com>
2025-07-04 09:51:21 +0700

    feat(keymap): update right encoder to play next/ prev song, and its button to play and pause

--- keymap.c
+++ keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, KC_ENT,      KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* LOWER
@@ -371,15 +371,15 @@ bool oled_task_user(void) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) { // Left encoder
-            tap_code(KC_VOLD); // Volume up
+            tap_code(KC_VOLD); // Volume down
         } else {
-            tap_code(KC_VOLU); // Volume down
+            tap_code(KC_VOLU); // Volume up
         }
     } else if (index == 1) { // Right encoder
         if (clockwise) {
-            tap_code(KC_PGDN); // Scroll down
+            tap_code(KC_MNXT); // Next track
         } else {
-            tap_code(KC_PGUP); // Scroll up
+            tap_code(KC_MPRV); // Prev track
         }
     }
     return false;
