168755a33db5c5231416b052b3bd544224932cc0
Jonathan0823 <eguinjonathan08@gmail.com>
2025-07-04 07:52:18 +0700

    chore: first commit

---
+++ config.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+
+#define TAPPING_TERM 500
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+
+#define OLED_TIMEOUT 120000
+#define OLED_BRIGHTNESS 120
+#define SPLIT_WPM_ENABLE
